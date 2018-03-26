9e8fc1838ea02b20f19aff4cc443cc51311bef9e
sonicrainboom7 <fullerdisaster@gmail.com>
2018-03-26 14:04:44 +0300

    A Basic gun that shoots a raycast and targets that take damage when the raycast hits them

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,34 @@
+﻿namespace VRTK.examples
+{
+    using UnityEngine;
+
+    public class Gun : VRTK_InteractableObject
+    {
+        public float damage = 10f;
+
+        public override void StartUsing(VRTK_InteractUse usingObject)
+        {
+            base.StartUsing(usingObject);
+            FireBullet();
+        }
+
+        protected void Start()
+        {
+           
+        }
+            // when the fire button is pressed the gun shoot a raycast and looks if it hit a target that can be damaged
+         void FireBullet()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, -transform.forward, out hit))
+            {
+                Debug.Log(hit.transform.name);
+                Target target = hit.transform.GetComponent<Target>();
+                if (target != null)
+                {
+                    target.TakeDamage(damage);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    public float health = 50f;
+
+
+    // when an object with health gets damaged
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+    // when an object with health reaches 0 hitpoints
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
