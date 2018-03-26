9e07edc8210eef2c79c429dfc6621cbe05b44ce1
sonicrainboom7 <fullerdisaster@gmail.com>
2018-03-26 10:23:20 +0300

    A grenade with a pullable pin that explodes. Has placeholders for the explosion effect and sound effect. Does no damage but pushes rigid bodies.

---
+++ Assets/Scripts/Grenade.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour {
+    public AudioClip explosionSound;
+    public float timer;
+    public float radius;
+    public float explosionForce;
+
+    bool timerStart = false;
+    public GameObject grenade;
+    public GameObject explosionEffect; 
+    
+
+    void Start()
+    {
+        
+    }
+	void Update () {
+        if (timerStart == true)
+        {
+            timer -= Time.deltaTime;
+        }
+        if (timer <= 0)
+        {
+            Explosion();
+            timerStart = false;
+            
+        }
+		
+	}
+    // When the pin is pulled
+    void OnJointBreak()
+    {
+        timerStart = true;
+    }
+    // grenade explosion
+    void Explosion()
+    {
+        AudioSource.PlayClipAtPoint(explosionSound, transform.position);
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+       Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach(Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, radius);
+            }
+        }
+        Destroy(grenade);
+
+    }
+    
+}
