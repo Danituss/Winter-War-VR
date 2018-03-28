b09d53624bfd963d419744a75c8da1f9cb50fc9c
Tuomas Juvonen <tuomasju@metropolia.fi>
2018-03-28 14:00:59 +0300

    Muzzleflash for the weapon (for real now)

---
+++ Assets/Scripts/MuzzleFlash.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuzzleFlash : MonoBehaviour {
+
+	public float maxLifetime;
+	public float lifetime;
+
+
+	// Use this for initialization
+	void Start () {
+		lifetime = maxLifetime;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		lifetime = lifetime - Time.deltaTime;
+		if (lifetime <= 0f) {
+			lifetime = maxLifetime;
+			gameObject.SetActive (false);
+		}
+	}
+}
