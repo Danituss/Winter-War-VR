a934ffdd5d6932dc606aadd8590f5ec3518e7543
sonicrainboom7 <fullerdisaster@gmail.com>
2018-04-03 15:13:03 +0300

    Started working on the player taking damage and the screen turning more red as the player takes damage

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public Image dmgOverlay;
+    float dmgTaken = 0f;
+    public Target healthCheck;
+    float maxHp;
+    
+
+	// Use this for initialization
+	void Start () {
+        dmgOverlay.canvasRenderer.SetAlpha(dmgTaken);
+        maxHp = healthCheck.health;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        dmgTaken = 1 - (healthCheck.health / maxHp);
+        Debug.Log(dmgTaken);
+        dmgOverlay.canvasRenderer.SetAlpha(dmgTaken);
+
+
+
+    }
+}
