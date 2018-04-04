395816dc255d0a4fe6926f7653e44593223fa58d
Dan <dan.huiko14@gmail.com>
2018-04-04 23:23:03 +0300

    So I made the grenade work straight with the VRTK, won't work anymore on the old grenade prefab.

--- Assets/Scripts/Grenade.cs
+++ Assets/Scripts/Grenade.cs
@@ -1,73 +1,78 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Diagnostics;
-
-public class Grenade : MonoBehaviour
+﻿namespace VRTK.examples
 {
+    using UnityEngine;
+    using System.Diagnostics;
 
-    public AudioClip explosionSound;
-
-    public float fuseTimer;
-
-    public float radius;
-    public float explosionForce;
-	public float damage;
-
-    public GameObject grenade;
-    public GameObject explosionEffect;
-
-    Stopwatch fuse;
-
-    void Start()
+    public class Grenade : VRTK_InteractableObject
     {
-        fuse = new Stopwatch();
-    }
 
-    void Update()
-    {
-        if (fuse.IsRunning && fuse.ElapsedMilliseconds >= fuseTimer * 1000f)
+        public AudioClip explosionSound;
+
+        public float fuseTimer;
+
+        public float radius;
+        public float explosionForce;
+        public float damage;
+
+        public GameObject grenade;
+        public GameObject explosionEffect;
+
+        Stopwatch fuse;
+
+        void Start()
         {
-            Explosion();
-            
+            fuse = new Stopwatch();
         }
-        
-    }
 
-    // When the pin is pulled
-    void OnJointBreak()
-    {
-        fuse.Start();
-    }
-
-    // grenade explosion
-    void Explosion()
-    {
-        AudioSource.PlayClipAtPoint(explosionSound, transform.position);
-		GameObject clone = Instantiate(explosionEffect, grenade.transform.position, grenade.transform.rotation);
-		Destroy (clone, 2);
-
-
-
-		foreach (Collider nearbyObject in Physics.OverlapSphere(grenade.transform.position, radius))
+        void Update()
         {
-            if (nearbyObject.GetComponent<Rigidbody>() != null)
+            if (fuse.IsRunning && fuse.ElapsedMilliseconds >= fuseTimer * 1000f)
             {
-                RaycastHit hit;
-				if (Physics.Raycast(grenade.transform.position, nearbyObject.transform.position - grenade.transform.position, out hit))
-                {
-					if (hit.collider == nearbyObject)
-						nearbyObject.GetComponent<Rigidbody>().AddExplosionForce(explosionForce, grenade.transform.position, radius);
-                    Target target = hit.transform.GetComponent<Target>();
-                    if (target != null)
-                    {
-						float distance = Vector3.Distance(target.transform.position, grenade.transform.position);
-						target.TakeDamage((radius-distance)*damage);
-                    }
-                }
-
+                Explosion();
+                Destroy(transform.parent.gameObject); //Destroy the pin after the explosion. Not the most elegant solution but it must be destroyed at some point.
             }
-			Destroy(grenade.transform.parent.gameObject);
+        }
+
+        /// <summary>
+        /// This method will be called when VRTK controller hovers on this' collider and presses trigger button.
+        /// </summary>
+        /// <param name="usingObject"></param>
+        public override void StartUsing(VRTK_InteractUse usingObject)
+        {
+            fuse.Start();
+            transform.parent.SetParent(null); //Put the pin to hierarchy root
+            transform.parent.GetComponent<Rigidbody>().isKinematic = false; //Set the pin's rigidbody to not kinematic
+        }
+
+        // grenade explosion
+        void Explosion()
+        {
+            AudioSource.PlayClipAtPoint(explosionSound, transform.position);
+            GameObject clone = Instantiate(explosionEffect, grenade.transform.position, grenade.transform.rotation);
+            Destroy(clone, 2);
+
+
+
+            foreach (Collider nearbyObject in Physics.OverlapSphere(grenade.transform.position, radius))
+            {
+                if (nearbyObject.GetComponent<Rigidbody>() != null)
+                {
+                    RaycastHit hit;
+                    if (Physics.Raycast(grenade.transform.position, nearbyObject.transform.position - grenade.transform.position, out hit))
+                    {
+                        if (hit.collider == nearbyObject)
+                            nearbyObject.GetComponent<Rigidbody>().AddExplosionForce(explosionForce, grenade.transform.position, radius);
+                        Target target = hit.transform.GetComponent<Target>();
+                        if (target != null)
+                        {
+                            float distance = Vector3.Distance(target.transform.position, grenade.transform.position);
+                            target.TakeDamage((radius - distance) * damage);
+                        }
+                    }
+
+                }
+                Destroy(grenade.transform.parent.gameObject);
+            }
         }
     }
 }
