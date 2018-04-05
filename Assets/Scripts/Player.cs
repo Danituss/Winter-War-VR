51c251a6bfd26f57194cee98ad22d8e780daf4af
sonicrainboom7 <fullerdisaster@gmail.com>
2018-04-05 13:35:04 +0300

    Updated damage taking system back to canvas. Maybe there is a function in multiplayer to make an object only visible by you?

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Player : MonoBehaviour {
@@ -9,41 +10,26 @@ public class Player : MonoBehaviour {
     float dmgTaken = 0f;
     public Target healthCheck;
     float maxHp;
-    private Color guiColor;
+    public RawImage damageEffect;
+    Color currColor;
 
-	/// <summary>
+    /// <summary>
     /// 
     /// </summary>
-	void Start () {
+    void Start () {
         
         maxHp = healthCheck.health;
-        guiColor = Color.white;
+        currColor = damageEffect.color;
 
     }
 	
-	// GUI Texture darkens as the player takes more damage
+	// Image on canvas that is in front of the player darkens as the player takes more damage
 	void Update () {
-        guiColor.a = dmgTaken - 0.1f;
+        currColor.a = dmgTaken - 0.05f;
         dmgTaken = 1 - (healthCheck.health / maxHp);
+        damageEffect.color = currColor;
         
-        
-
-        
-    }
-    public Vector3 WorldToGuiPoint(Vector3 position)
-    {
-        var guiPosition = Camera.main.WorldToScreenPoint(position);
-        guiPosition.y = Screen.height - guiPosition.y;
-
-        return guiPosition;
-    }
-    /// <summary>
-    /// draws the damageTexture
-    /// </summary>
-    void OnGUI()
-    {
-        GUI.color = guiColor;
-        GUI.DrawTexture(new Rect(0, 0,Screen.width, Screen.height), dmgOverlay);
-        
-    }
+        }
+   
+    
 }
