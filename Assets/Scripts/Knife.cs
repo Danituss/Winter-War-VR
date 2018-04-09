7cd0c5bab1a3cbed85fb330484e1e96e0e73190a
sonicrainboom7 <fullerdisaster@gmail.com>
2018-04-09 10:04:20 +0300

    Made a simple base for a knife object.

---
+++ Assets/Scripts/Knife.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour {
+    public float damage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter(Collision col)
+    {
+        
+        Target target = col.gameObject.GetComponent<Target>();
+        if (target != null)
+        {
+            Debug.Log(col);
+            target.TakeDamage(damage);
+        }
+    }
+}
