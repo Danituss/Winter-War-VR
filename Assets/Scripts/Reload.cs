62de7dd219ceab20f5fb17f3e87951f2a17f1dfd
sonicrainboom7 <fullerdisaster@gmail.com>
2018-04-11 10:58:48 +0300

    Started work on the reloading system. You can drop ammo on a specified spot on the gun to regain all ammo.

---
+++ Assets/Scripts/Reload.cs
@@ -0,0 +1,38 @@
+﻿namespace VRTK
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    using UnityEngine;
+
+    public class Reload : MonoBehaviour
+    {
+        GameObject gun;
+        Gun gunscript;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            gun = transform.parent.gameObject;
+            gunscript = gun.GetComponent<Gun>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.Log(gun);
+
+        }
+        void OnTriggerEnter(Collider col)
+        {
+            
+            if (col.gameObject.tag == "Ammo")
+            {
+                Debug.Log("toimiiko?");
+                gunscript.ammo = 5;
+                Destroy(col.gameObject);
+            }
+        }
+    }
+}
