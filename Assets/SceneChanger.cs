1e6e82e7d32b0038389817d4181d390cd09682c1
Dan <dan.huiko14@gmail.com>
2018-04-16 23:48:42 +0300

    Way too much changes to write it all down after 15h of work :D sorry.

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > 15f)
+        {
+            SceneManager.LoadScene(1);
+        }
+	}
+}
