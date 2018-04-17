79c3645665066b929f77315e8efca445b0e04a80
sonicrainboom7 <fullerdisaster@gmail.com>
2018-04-17 12:24:19 +0300

    Updates to the bolt rotation

---
+++ Assets/Scripts/BoltLock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoltLock : MonoBehaviour {
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        
+       if (this.gameObject.transform.rotation.eulerAngles.z > 270)
+        {
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            rb.constraints &= ~RigidbodyConstraints.FreezeRotationZ;
+            rb.constraints &= ~RigidbodyConstraints.FreezeRotationY;
+            rb.constraints &= ~RigidbodyConstraints.FreezeRotationX;
+        }
+       else
+        {
+            rb.constraints &= ~RigidbodyConstraints.FreezePositionZ;
+        }
+		
+	}
+}
