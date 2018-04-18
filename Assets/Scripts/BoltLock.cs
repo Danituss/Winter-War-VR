ab29806dde023d6ca28e45b7b24f5a835283d955
sonicrainboom7 <fullerdisaster@gmail.com>
2018-04-18 11:32:21 +0300

    Small adjustments. Increasing the strength of spring grab attach increases the bolts movement speed.

--- Assets/Scripts/BoltLock.cs
+++ Assets/Scripts/BoltLock.cs
@@ -19,7 +19,7 @@ public class BoltLock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(atTheBack);
+        
 
         //Lock movement when bolt isn't upright
         if (rb.rotation.eulerAngles.z > 270)
