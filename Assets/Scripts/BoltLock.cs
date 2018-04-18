830564077f612a92b38394c94dd4c8d036f34138
sonicrainboom7 <fullerdisaster@gmail.com>
2018-04-18 11:10:11 +0300

    First working version of the bolt action. Works really bad in VR and code is super spaghetti

--- Assets/Scripts/BoltLock.cs
+++ Assets/Scripts/BoltLock.cs
@@ -2,30 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoltLock : MonoBehaviour {
+public class BoltLock : MonoBehaviour
+{
     Rigidbody rb;
+    Vector3 startPos;
+    bool atTheBack;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rb = this.gameObject.GetComponent<Rigidbody>();
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-        
-       if (this.gameObject.transform.rotation.eulerAngles.z > 270)
+        startPos = transform.localPosition;
+        atTheBack = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(atTheBack);
+
+        //Lock movement when bolt isn't upright
+        if (rb.rotation.eulerAngles.z > 270)
         {
             rb.constraints = RigidbodyConstraints.FreezeAll;
             rb.constraints &= ~RigidbodyConstraints.FreezeRotationZ;
             rb.constraints &= ~RigidbodyConstraints.FreezeRotationY;
             rb.constraints &= ~RigidbodyConstraints.FreezeRotationX;
+
+
         }
-       else
+        //Unlock movement when bolt is upright
+        else if (this.gameObject.transform.localPosition.z < startPos.z && atTheBack == false || rb.rotation.eulerAngles.z < 270 && atTheBack == false)
         {
+            rb.constraints = RigidbodyConstraints.FreezeRotationZ;
             rb.constraints &= ~RigidbodyConstraints.FreezePositionZ;
+            rb.constraints &= ~RigidbodyConstraints.FreezePositionY;
+            rb.constraints &= ~RigidbodyConstraints.FreezePositionX;
+
+
         }
-		
-	}
+
+
+
+    }
+    //When the bolt reaches the back of the weapon, it can move down
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.name == "BoltLock Trigger Back")
+        {
+
+            if (atTheBack == false)
+            {
+                atTheBack = true;
+                rb.constraints &= ~RigidbodyConstraints.FreezeRotationZ;
+            }
+            else
+            {
+                atTheBack = false;
+            }
+
+        }
+        if (col.gameObject.name == "BoltLock Trigger Front")
+        {
+
+            if (atTheBack == false)
+            {
+                atTheBack = true;
+                rb.constraints &= ~RigidbodyConstraints.FreezeRotationZ;
+            }
+            else
+            {
+                atTheBack = false;
+            }
+
+        }
+
+    }
 }
