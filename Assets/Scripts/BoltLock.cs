e5b4ef5d543662dd4491d6205a48762d12be0444
sonicrainboom7 <fullerdisaster@gmail.com>
2018-04-19 15:43:06 +0300

    More fine tuning for the bolt. Track Object Grab Attach seems to be working the best

--- Assets/Scripts/BoltLock.cs
+++ Assets/Scripts/BoltLock.cs
@@ -14,14 +14,13 @@ public class BoltLock : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody>();
         startPos = transform.localPosition;
         upRight = false;
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(upRight);
-
 
         //Lock movement when bolt isn't upright
         if (rb.rotation.eulerAngles.z > 270 || upRight == false)
@@ -30,11 +29,11 @@ public class BoltLock : MonoBehaviour
             rb.constraints &= ~RigidbodyConstraints.FreezeRotationZ;
             rb.constraints &= ~RigidbodyConstraints.FreezeRotationY;
             rb.constraints &= ~RigidbodyConstraints.FreezeRotationX;
-
+         
 
         }
         //Unlock movement when bolt is upright
-        else if (gameObject.transform.localPosition.z < startPos.z || rb.rotation.eulerAngles.z < 270 && upRight == true)
+        else if (gameObject.transform.localPosition.z < startPos.z && upRight == true || rb.rotation.eulerAngles.z < 270 && upRight == true)
         {
             rb.constraints = RigidbodyConstraints.FreezeRotationZ;
             rb.constraints &= ~RigidbodyConstraints.FreezePositionZ;
@@ -50,25 +49,23 @@ public class BoltLock : MonoBehaviour
     //When the bolt reaches the back of the weapon, it can move down
     void OnTriggerEnter(Collider col)
     {
-
-
+        
+        
         if (col.gameObject.name == "BoltLock Trigger Front")
         {
-
-            
-            if (upRight == false)
-            {
+                
+                
+            if (upRight == false) {
                 upRight = true;
             }
-            else
+            else if(upRight == true)
             {
                 upRight = false;
             }
+          
+
         }
-
-
+   
     }
-
-}
-    
+    }
 
