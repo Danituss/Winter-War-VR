499e69d8440f270389a05997c9d812f4e83a6953
Huiko Dan <dan.huiko@yle.fi>
2018-04-20 12:45:53 +0300

    New script to handle clip snap drop zone stuff.

---
+++ Assets/Scripts/ClipSnapDropZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRTK
+{
+    public class ClipSnapDropZone : VRTK_SnapDropZone
+    {
+        public void DestroyConnectedClip()
+        {
+            Destroy(currentSnappedObject);
+        }
+    }
+}
