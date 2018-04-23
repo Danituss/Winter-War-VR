548e4bb9834d2a8367d3b8980a3f41632e32a5f5
sonicrainboom7 <fullerdisaster@gmail.com>
2018-04-23 10:13:48 +0300

    Most stable version of the bolts movement at the moment

--- Assets/Scripts/BoltLock.cs
+++ Assets/Scripts/BoltLock.cs
@@ -7,65 +7,53 @@ public class BoltLock : MonoBehaviour
     Rigidbody rb;
     Vector3 startPos;
     bool upRight;
+   
 
     // Use this for initialization
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
-        startPos = transform.localPosition;
         upRight = false;
+        rb = gameObject.GetComponent<Rigidbody>();
         
+        SideWays();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(upRight);
-
-        //Lock movement when bolt isn't upright
-        if (rb.rotation.eulerAngles.z > 270 || upRight == false)
-        {
-            rb.constraints = RigidbodyConstraints.FreezeAll;
-            rb.constraints &= ~RigidbodyConstraints.FreezeRotationZ;
-            rb.constraints &= ~RigidbodyConstraints.FreezeRotationY;
-            rb.constraints &= ~RigidbodyConstraints.FreezeRotationX;
-         
-
-        }
-        //Unlock movement when bolt is upright
-        else if (gameObject.transform.localPosition.z < startPos.z && upRight == true || rb.rotation.eulerAngles.z < 270 && upRight == true)
-        {
-            rb.constraints = RigidbodyConstraints.FreezeRotationZ;
-            rb.constraints &= ~RigidbodyConstraints.FreezePositionZ;
-            rb.constraints &= ~RigidbodyConstraints.FreezePositionY;
-            rb.constraints &= ~RigidbodyConstraints.FreezePositionX;
-
-
-        }
-
-
-
     }
     //When the bolt reaches the back of the weapon, it can move down
     void OnTriggerEnter(Collider col)
     {
-        
-        
-        if (col.gameObject.name == "BoltLock Trigger Front")
+
+        // call when bolt is coming from the sideways position to upright position
+        if (col.gameObject.name == "BoltLock Trigger Front" && upRight == false)
         {
-                
-                
-            if (upRight == false) {
-                upRight = true;
-            }
-            else if(upRight == true)
-            {
-                upRight = false;
-            }
-          
-
+            Upwards();
+            upRight = true;
+        // called when bolt is coming from the back of the weapon back to the front
+        } else if (col.gameObject.name == "BoltLock Trigger Front" && upRight == true) {
+            SideWays();
+            upRight = false;
         }
-   
+       
     }
+    void SideWays()
+    {
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+        rb.constraints &= ~RigidbodyConstraints.FreezeRotationZ;
+        rb.constraints &= ~RigidbodyConstraints.FreezeRotationY;
+        rb.constraints &= ~RigidbodyConstraints.FreezeRotationX;
+        
     }
-
+    void Upwards()
+    {
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+        rb.constraints &= ~RigidbodyConstraints.FreezePositionZ;
+        rb.constraints &= ~RigidbodyConstraints.FreezePositionY;
+        rb.constraints &= ~RigidbodyConstraints.FreezePositionX;
+       
+    }
+}
