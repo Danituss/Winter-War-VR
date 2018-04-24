c0294cc24f376160fb409d35e208ebde45ee6f37
sonicrainboom7 <fullerdisaster@gmail.com>
2018-04-24 12:24:58 +0300

    Started setting up a multiplayer environment. Not sure if it works yet.

---
+++ Assets/Scripts/ViveManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveManager : MonoBehaviour {
+    public GameObject head;
+    public GameObject leftHand;
+    public GameObject rightHand;
+
+    public static ViveManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+    }
+    void OnDestroy() {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
