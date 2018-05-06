656568f861517c03f33dbe946587ad773619e3f6
Iiro Kiema <ikiema@hotmail.com>
2018-05-07 00:43:19 +0300

    2D lobby system, work-in-progress. (I will try to do a proper 3D lobby system later if there is enough time.)

---
+++ Assets/Scripts/CreateRoom/CreateRoom.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateRoom : MonoBehaviour {
+    [SerializeField]
+    private Text _roomName;
+    private Text RoomName
+    {
+        get { return _roomName; }
+    }
+
+    public void OnClick_CreateRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 4 };
+
+        if (PhotonNetwork.CreateRoom(RoomName.text, roomOptions, TypedLobby.Default))
+        {
+            Debug.Log("Create Room succesfully sent.");
+        }
+        else
+        {
+            Debug.Log("Create Room failed to send");
+        }
+    }
+
+    private void OnPhotonCreateRoomFailed(object[] codeAndMessage)
+    {
+        Debug.Log("Create Room failed: " + codeAndMessage[1]);
+    }
+
+    private void OnCreatedRoom()
+    {
+        Debug.Log("Room created succesfully.");
+    }
+}
---
+++ Assets/Scripts/Networks/PlayerNetwork.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerNetwork : MonoBehaviour {
+
+    public static PlayerNetwork Instance;
+    public string PlayerName { get; private set; }
+
+	// Use this for initialization
+	private void Awake () {
+        Instance = this;
+        PlayerName = "Distul#" + Random.Range(1000, 9999);
+	}
+}
